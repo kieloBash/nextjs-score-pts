6a28f2da3998da4ff1e207532d134762b0529f75
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2023-12-26 17:37:21 +0800

    added totals

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Display from "@/components/display";
 import { FormPlayer } from "@/components/form";
+import TotalDisplay from "@/components/total";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-10 bg-gradient-to-r from-blue-800 to-indigo-900">
-      <div className="mb-4 text-4xl font-black text-center text-white lg:mb-8 lg:text-8xl">
-        Player Scores
-      </div>
+      <TotalDisplay />
       <Display />
       {/* <FormPlayer /> */}
     </main>
